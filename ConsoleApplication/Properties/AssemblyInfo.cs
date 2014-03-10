942f802d51720a114097643d1797173a3bff07bd
Benoît Blanchon <benoit.blanchon@satimo.fr>
2014-03-10 14:06:54 +0100

    Removed irrelevant assembly info

--- ConsoleApplication/Properties/AssemblyInfo.cs
+++ ConsoleApplication/Properties/AssemblyInfo.cs
@@ -1,9 +1,4 @@
-﻿/*
- * This file is part of the Satimo Software Framework
- *
- * Copyright © Satimo 2014 All Rights Reserved, http://satimo.fr/
- */
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -11,18 +6,6 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ConsoleApplication")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Satimo")]
-[assembly: AssemblyProduct("ConsoleApplication")]
-[assembly: AssemblyCopyright("Copyright © Satimo 2014")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("2e2c5b18-7710-47ef-bb0e-ffd12ba2e19d")]
